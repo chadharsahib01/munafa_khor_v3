fb37b6e60a7686ced14123f5e01940deb9c96ce3
chadharsahib01 <casualloginn@gmail.com>
2025-05-26 10:55:42 +0500

    Create screens/dashboard.dart

---
+++ lib/screens/dashboard.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class DashboardScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Munafa Khor Dashboard")),
+      body: Center(child: Text("Investment calculator will be here.")),
+    );
+  }
+}
